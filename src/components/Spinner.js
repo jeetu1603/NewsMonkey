e09ec40eee64872ab2b8025b5f59e64386195f6a
Jeetu Bangari <jbangari16@gmail.com>
2022-01-24 19:24:01 +0530

    Changed class components to function components & made navbar sticky

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
-
-export class Spinner extends Component {
-  render() {
-    return <div className="d-flex justify-content-center my-3">
-    <div className="spinner-border" role="status">
-      <span className="visually-hidden">Loading...</span>
+const Spinner = () => {
+  return (
+    <div className="d-flex justify-content-center">
+      <div className="spinner-border my-3" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
     </div>
-  </div>;
-  }
-}
+  );
+};
 
 export default Spinner;
