d2cfe725f73d64ac92b8529ed9ca818838c34914
Jeetu Bangari <jbangari16@gmail.com>
2022-01-24 11:04:29 +0530

    Added Infinite ScrollBar to News Component

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
   static defaultProps = {
@@ -24,8 +25,9 @@ export default class News extends Component {
     super(props);
     this.state = {
       articles: [],
-      loading: false,
-      page: 1
+      loading: true,
+      page: 1,
+      totalResults: 0
     }
     document.title = `${this.capitalizeFirstLetter(this.props.category)} | NewsMonkey`;
   }
@@ -40,37 +42,32 @@ export default class News extends Component {
         loading: false});
 }
 
- handlePrevClick = async ()=>{
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f2839c58f8d6418e844c0f1df112d4fb&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading: true});
+  fetchMoreData = async () => {  
+    this.setState({page: this.state.page + 1})
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f2839c58f8d6418e844c0f1df112d4fb&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
-    let parsedData = await data.json();
+    let parsedData = await data.json()
     this.setState({
-        page: this.state.page - 1,
-        articles: parsedData.articles,
-        loading: false
-    });
-}
-
- handleNextClick = async ()=>{
-    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=f2839c58f8d6418e844c0f1df112d4fb&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading: true});
-        let data = await fetch(url);
-        let parsedData = await data.json() 
-        this.setState({
-            page: this.state.page + 1,
-            articles: parsedData.articles,
-            loading: false
-        });
-    }
-  }
+        articles: this.state.articles.concat(parsedData.articles),
+        totalResults: parsedData.totalResults
+    })
+  };
+  
 
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h1 className="text-center" style={{margin: '35px 0'}}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
         {this.state.loading && <Spinner/>}
+
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        >
+
+        <div className="container">
         <div className="row">
           {!this.state.loading && this.state.articles.map((element)=>{
             return (<div className="col-md-4" key={element.url}>
@@ -78,11 +75,11 @@ export default class News extends Component {
           </div>);
           })}
         </div>
-        <div className="d-flex justify-content-between">
-        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-        <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
-      </div>
+        
+        </InfiniteScroll>
+
+        </>
     );
   }
 }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export class Spinner extends Component {
   render() {
-    return <div className="d-flex justify-content-center">
+    return <div className="d-flex justify-content-center my-3">
     <div className="spinner-border" role="status">
       <span className="visually-hidden">Loading...</span>
     </div>
